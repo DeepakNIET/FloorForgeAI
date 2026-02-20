8d6388bcf7c93e4a0dc6450c5960c9bcd72d8c2b
Deepak <141200171+DeepakNIET@users.noreply.github.com>
2026-02-20 16:19:24 +0530

    Navbar.tsx

--- floorforge-main/components/Navbar.tsx
+++ floorforge-main/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                         <Box  className="logo" />
 
                         <span className="name">
-                            Roomify
+                            FloorForge-AI
                         </span>
                     </div>
 
